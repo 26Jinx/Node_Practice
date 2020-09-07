bc089ccdcf59548cbf74fe9a497ceba1222b12a9
PharahMain <26randomforests@gmail.com>
2020-09-06 20:05:41 -0700

    filter with cb practice

---
+++ filtered-ls.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const { pathToFileURL } = require('url')
+const directory = process.argv[2]
+const fileExt = process.argv[3]
+
+fs.readdir(directory, function doneReading(err, list) {
+    for (const f of list) {
+        if (path.extname(f) == "." + fileExt) {
+            console.log(f)
+        }
+    }
+})
