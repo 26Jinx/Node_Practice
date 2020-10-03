0d15b321b28b1e766ac4e707123dea63e134cf06
PharahMain <26randomforests@gmail.com>
2020-10-02 21:20:10 -0700

    parsing request url

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -9,13 +9,16 @@ let server = http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json' });
 
     // parse request url
-    let myUrl = new URL(request.url.toString());
-    response.end(myUrl);
+    let parsedUrl = url.parse(request.url, true);
 
     // first endpoint
-
+    if (parsedUrl.pathname.includes('parsetime')) {
+        response.end(console.log(parsedUrl.query.iso));
+    }
     // second endpoint
-    // console.log(new URL('/test?q=1', 'http://example.com'));
+    if (parsedUrl.pathname.includes('unixtime')) {
+        response.end(console.log(parsedUrl.query.iso));
+    }
 })
 
 server.listen(port);
